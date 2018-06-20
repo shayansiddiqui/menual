99f4a2e31955df5e405046927c521dc478fc4e34
chart21 <chart21@web.de>
2018-06-20 02:42:23 +0200

    addded positive and negatibe comments for scanned dishes like "Great, this meal contains much protein"

--- app/src/main/java/de/fbl/menual/MainActivity.java
+++ app/src/main/java/de/fbl/menual/MainActivity.java
@@ -25,9 +25,11 @@ import java.text.SimpleDateFormat;
 
 import android.util.Base64;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import java.util.Arrays;
 
 
 import org.json.JSONObject;
@@ -69,10 +71,10 @@ public class MainActivity extends AppCompatActivity {
 
 //                TODO: Just a sample call. Need to move from here
 
-                getNutrition("Big mac");
-                getNutrition("Salmon salad");
-                getNutrition("Pizza");
-                getNutrition("Spaghetti bolognese");
+              //getNutrition("Big mac");
+               getNutrition("Salmon salad");
+               getNutrition("Pizza");
+               //getNutrition("Spaghetti bolognese");
 
 
             }
@@ -257,15 +259,16 @@ public class MainActivity extends AppCompatActivity {
                 //JsonObject lresponse = response.body();
                 //System.out.println(response.body().toString());
                 String sApiValues = response.body().toString();
+
+
+                //System.out.println(b);
                 double[] apiValues = new double[32];
                 String a = "";
                 String[] splitApiValues = sApiValues.split("full_nutrients", -1);
-                String[] subHaupt =  splitApiValues[0].split(",",-1);
+                String[] subHaupt = splitApiValues[0].split(",", -1);
                 String[] inhalt = {"nf_protein", "nf_total_fat", "nf_total_carbohydrate", "nf_sugars", "nf_dietary_fiber", "nf_saturated_fat"};
-                for(int i = 0; i<subHaupt.length;i++)
-                {
-                    for (int j = 0; j < inhalt.length; j++)
-                    {
+                for (int i = 0; i < subHaupt.length; i++) {
+                    for (int j = 0; j < inhalt.length; j++) {
                         if (subHaupt[i].contains(inhalt[j])) {
                             if (subHaupt[i].substring(inhalt[j].length() + 3).equals("null"))
                                 apiValues[j] = -1;
@@ -274,58 +277,139 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 }
-                String[] subExtra =  splitApiValues[1].split("\\}",-1);
-                String [] inhaltExtra = {"attr_id\":645,","attr_id\":646,"}; //645 monosaturated, 646 polysaturated
-                for(int i = 0; i<subExtra.length;i++)
-                {
-                    for (int j = 0; j < inhaltExtra.length; j++)
-                    {
+                String[] subExtra = splitApiValues[1].split("\\}", -1);
+                String[] inhaltExtra = {"attr_id\":645,", "attr_id\":646,"}; //645 monosaturated, 646 polysaturated
+                for (int i = 0; i < subExtra.length; i++) {
+                    for (int j = 0; j < inhaltExtra.length; j++) {
                         if (subExtra[i].contains(inhaltExtra[j])) {
                             if (subExtra[i].substring(inhaltExtra[j].length() + 11).equals("null"))
-                                apiValues[j+6] = -1;
+                                apiValues[j + 6] = -1;
                             else
-                                apiValues[j+6] = Double.parseDouble(subExtra[i].substring(inhaltExtra[j].length() + 11));
+                                apiValues[j + 6] = Double.parseDouble(subExtra[i].substring(inhaltExtra[j].length() + 11));
                         }
                     }
                 }
 
 
-
-
                 //Test Code
-                String s ="";
-                System.out.println("Food result for: " +foodName);
+                String s = "";
+                System.out.println("Food result for: " + foodName);
                 System.out.println("The dish contains the following nutrients");
-                for(double i:apiValues)
-                    s += Double.toString(i) +"\n";
-                for(int i = 8;i<apiValues.length;i++)
+                for (double i : apiValues)
+                    s += Double.toString(i) + "\n";
+                for (int i = 8; i < apiValues.length; i++)
                     apiValues[i] = 0;
                 System.out.println(s);
                 Evaluator e = new Evaluator();
-                int[] preferences ={1,1,1,1,1};
+                int[] preferences = {1, 1, 1, 1, 1};
                 System.out.println();
                 System.out.println("The dish receives the following scores");
-                int scores[] = e.evaluateDish(1,preferences,apiValues);
-                for(int i=0;i<scores.length;i++)
-                System.out.println(scores[i]);
+                int scores[] = e.evaluateDish(1, preferences, apiValues);
+                for (int i = 0; i < scores.length; i++)
+                    System.out.println(scores[i]);
                 System.out.println();
                 System.out.println("The dish receives the following colour");
-                if(scores[0] > 100)
+                int[] sortedScores = scores.clone();
+                Arrays.sort(sortedScores);
+
+                String[] ingredients = {"", "proteins", "sugar", "fiber", "healthy fats", "vitamins"};
+                String[] ingredientsUnhealthy = {"", "proteins", "sugar", "fiber", "saturated fats", "vitamins"};
+
+                if (scores[0] > 100) {
                     System.out.println("green");
-                else {
-                    if (scores[0] > 90)
+                    int max = sortedScores[sortedScores.length - 1];
+                    int max2 = sortedScores[sortedScores.length - 2];
+
+                    if (max > 110) {
+                        int result1st = e.indexOf(scores,max);
+                        String kriterium1 = ingredients[result1st];
+                        if (result1st != 0)
+                            System.out.println("Awesome, this meal contains a lot of " + kriterium1 + "!");
+                    }
+                    if (max2 > 100) {
+                        int result2nd = e.indexOf(scores,max2);
+                        if(result2nd == 0)
+                        {
+                            max2 = sortedScores[sortedScores.length - 3];
+                            if(max2 > 100)
+                            result2nd = e.indexOf(scores,max2);
+                        }
+                        String kriterium2 = ingredients[result2nd];
+                        if (result2nd != 0)
+                            System.out.println("Great, this meal contains a lot of " + kriterium2 + "!");
+                    }
+
+                } else {
+                    if (scores[0] > 90) {
                         System.out.println("yellow");
-                    else
+                    } else {
                         System.out.println("red");
+                    }
+                    int min = sortedScores[0];
+                    int min2 = sortedScores[1];
+                    if (min < 87) {
+                        //int result1st = Arrays.asList(scores).indexOf(min);
+                        int result1st = e.indexOf(scores, min);
+                        String kriterium1 = ingredientsUnhealthy[result1st];
+                        if (result1st != 0) {
+                            if (result1st == 2)
+                                System.out.println("Boo, this meal contains too much " + kriterium1 + "!");
+                            if (result1st == 4)
+                                System.out.println("Oh no, this meal contains too many " + kriterium1 + "!");
+                            if (result1st == 3 || result1st == 5)
+                                System.out.println("This meal contains not enough " + kriterium1 + "!");
+                            if (result1st == 1) {
+                                if (e.getDetails()[0] == 1) {
+                                    System.out.println("This meal has too many carbohydrates!");
+                                }
+                                if (e.getDetails()[1] == 1) {
+                                    System.out.println("This meal has too many fats!");
+                                }
+                                int[][] mahlzeit = e.getMahlzeit();
+                                int mealtime = 1; //Has to be replaced with actual mealtime
+                                String[] mealtype = {"breakfast", "lunch", "dinner", "snack"};
+                                System.out.println("The optimal " + mealtype[mealtime] + " should only consist of a maximum proportion of " + mahlzeit[mealtime][3] + "% fat and at most " + mahlzeit[mealtime][4] + "% carbohydrates!");
+                            }
+                        }
                     }
 
 
+                    if (min2 < 87) {
+                        int result2nd = e.indexOf(scores, min2);
+
+                        if(result2nd == 0) {
+                            min2 = sortedScores[2] ;
+                            if(min2<87)
+                            result2nd = e.indexOf(scores,min2);
+                        }
+                        String kriterium2 = ingredientsUnhealthy[result2nd];
+
+                        if (result2nd != 0) {
+
+                                if (result2nd == 2)
+                                    System.out.println("Bad news, this meal contains too much " + kriterium2 + "!");
+                                if (result2nd == 4)
+                                    System.out.println("Bad news, this meal contains too many " + kriterium2 + "!");
+                                if (result2nd == 3 || result2nd == 5)
+                                    System.out.println("Unfortunately, this meal contains not enough " + kriterium2 + "!");
+                            }
+
+                        if (result2nd == 1) {
+                            if (e.getDetails()[0] == 1) {
+                                System.out.println("This meal has too many carbohydrates!");
+                            }
+                            if (e.getDetails()[1] == 1) {
+                                System.out.println("This meal has too many fats!");
+                            }
+                            int[][] mahlzeit = e.getMahlzeit();
+                            int mealtime = 1; //Has to be replaced with actual mealtime
+                            String[] mealtype = {"breakfast", "lunch", "dinner", "snack"};
+                            System.out.println("The optimal " + mealtype[mealtime] + " should only consist of a maximum proportion of " + mahlzeit[mealtime][3] + "% fat and at most " + mahlzeit[mealtime][4] + "% carbohydrates!");
+                        }
 
 
-
-
-
-
+                    }
+                }
 
 
 
