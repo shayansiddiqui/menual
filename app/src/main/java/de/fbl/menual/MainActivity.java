4f053507c872a4590f93a354cf617370f05691ca
Shayan Ahmad Siddiqui <shayan.siddiqui89@gmail.com>
2018-06-20 22:02:21 +0200

    adds the food result functionality after text detection

--- app/src/main/java/de/fbl/menual/MainActivity.java
+++ app/src/main/java/de/fbl/menual/MainActivity.java
@@ -1,5 +1,6 @@
 package de.fbl.menual;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,34 +11,28 @@ import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.JsonReader;
+import android.util.Base64;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-
-import android.util.Base64;
-
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import java.util.Arrays;
-
-
-import org.json.JSONObject;
-
 import java.util.Date;
+import java.util.List;
 
-import de.fbl.menual.api.RetrofitInstance;
 import de.fbl.menual.api.ApiInterface;
+import de.fbl.menual.api.RetrofitInstance;
 import de.fbl.menual.utils.CameraPreview;
 import de.fbl.menual.utils.Constants;
 import de.fbl.menual.utils.Evaluator;
@@ -68,15 +63,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mCamera.takePicture(null, null, mPicture);
-
-//                TODO: Just a sample call. Need to move from here
-
-                getNutrition("Big mac");
-               getNutrition("Salmon salad");
-               getNutrition("Pizza");
-               getNutrition("Spaghetti bolognese");
-
-
             }
         });
 
@@ -88,6 +74,10 @@ public class MainActivity extends AppCompatActivity {
         //*EDIT*//params.setFocusMode("continuous-picture");
         //It is better to use defined constraints as opposed to String, thanks to AbdelHady
         params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+
+        Camera.Size desiredSize = getPictureSize(params.getSupportedPictureSizes());
+        System.out.println(desiredSize.width);
+        params.setPictureSize(desiredSize.width, desiredSize.height);
         mCamera.setParameters(params);
 
 
@@ -104,6 +94,17 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    private Camera.Size getPictureSize(List<Camera.Size> sizes) {
+
+        for (Camera.Size size : sizes) {
+            if ((size.width * size.height) / 1024000 <= 2.5) {
+                return size;
+            }
+        }
+
+        return null;
     }
 
     @Override
@@ -138,12 +139,12 @@ public class MainActivity extends AppCompatActivity {
         return c; // returns null if camera is unavailable
     }
 
-
     private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+        private ProgressDialog mDialog;
 
         @Override
-        public void onPictureTaken(byte[] data, Camera camera) {
-
+        public void onPictureTaken(byte[] data, final Camera camera) {
+            mDialog = ProgressDialog.show(MainActivity.this, "In progress", "Detecting text...", true);
             BitmapFactory.Options bounds = new BitmapFactory.Options();
             bounds.inJustDecodeBounds = true;
             BitmapFactory.decodeByteArray(data, 0, data.length, bounds);
@@ -152,9 +153,9 @@ public class MainActivity extends AppCompatActivity {
             Matrix matrix = new Matrix();
 
             matrix.postRotate(rotation, (float) bm.getWidth() / 2, (float) bm.getHeight() / 2);
-            Bitmap rotatedBitmap = Bitmap.createBitmap(bm, 0, 0, bounds.outWidth, bounds.outHeight, matrix, true);
+            final Bitmap rotatedBitmap = Bitmap.createBitmap(bm, 0, 0, bounds.outWidth, bounds.outHeight, matrix, true);
 
-            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            final File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
 
             if (pictureFile == null) {
                 Log.d(TAG, "Error creating media file, check storage permissions: ");
@@ -167,46 +168,59 @@ public class MainActivity extends AppCompatActivity {
                 rotatedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
                 fos.write(data);
                 fos.close();
-                String encoded = Base64.encodeToString(data, Base64.DEFAULT);
-
-                JsonObject httpQuery = new JsonParser().parse(
-                        "{" +
-                                "\"requests\": [" +
-                                "{" +
-                                "\"image\":{" +
-                                "\"content\":" + "\"" + encoded + "\"" +
-                                "}, " +
-                                "\"features\":[" +
-                                "{" +
-                                "\"type\":\"" + Constants.OCR_TYPE + "\"" +
-                                "}" +
-                                "]" +
-                                "}" +
-                                "]" +
-                                "}").getAsJsonObject();
-
-                Callback<JsonObject> callbackTextDetection = new Callback<JsonObject>() {
-                    @Override
-                    public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                        System.out.println(response.body());
-                    }
-
-                    @Override
-                    public void onFailure(Call<JsonObject> call, Throwable t) {
-                        System.out.println(t.getMessage());
-                    }
-                };
-
-                Call<JsonObject> callTextDetection = apiInterface.detectText(httpQuery.toString());
-                callTextDetection.enqueue(callbackTextDetection);
-
             } catch (FileNotFoundException e) {
                 Log.d(TAG, "File not found: " + e.getMessage());
             } catch (IOException e) {
                 Log.d(TAG, "Error accessing file: " + e.getMessage());
             }
+
+            String encoded = convertToBase64(rotatedBitmap);
+
+            JsonObject httpQuery = new JsonParser().parse(
+                    "{" +
+                            "\"requests\": [" +
+                            "{" +
+                            "\"image\":{" +
+                            "\"content\":" + "\"" + encoded + "\"" +
+                            "}, " +
+                            "\"features\":[" +
+                            "{" +
+                            "\"type\":\"" + Constants.OCR_TYPE + "\"" +
+                            "}" +
+                            "]" +
+                            "}" +
+                            "]" +
+                            "}").getAsJsonObject();
+
+            Callback<JsonObject> callbackTextDetection = new Callback<JsonObject>() {
+                @Override
+                public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                    Intent myIntent = new Intent(MainActivity.this, TextSelection.class);
+                    myIntent.putExtra(Constants.PREVIEW_IMAGE_KEY, pictureFile); //Optional parameters
+                    myIntent.putExtra(Constants.DETECTION_RESPONSE_KEY, response.body().toString()); //Optional parameters
+                    MainActivity.this.startActivity(myIntent);
+                    mDialog.dismiss();
+                    System.out.println(response.body());
+                }
+
+                @Override
+                public void onFailure(Call<JsonObject> call, Throwable t) {
+                    System.out.println(t.getMessage());
+                }
+            };
+
+            Call<JsonObject> callTextDetection = apiInterface.detectText(httpQuery.toString());
+            callTextDetection.enqueue(callbackTextDetection);
         }
     };
+
+
+    private static String convertToBase64(Bitmap bitmap) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+
+        return Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT);
+    }
 
     /**
      * Create a File for saving an image or video
@@ -244,206 +258,5 @@ public class MainActivity extends AppCompatActivity {
         return mediaFile;
     }
 
-    public void getComment(int[] scores, Evaluator e)
-    {
-        int[] sortedScores = scores.clone();
-        Arrays.sort(sortedScores);
 
-        String[] ingredients = {"", "proteins", "sugar", "fiber", "healthy fats", "vitamins"};
-        String[] ingredientsUnhealthy = {"", "proteins", "sugar", "fiber", "saturated fats", "vitamins"};
-
-
-        String comment1 = "";
-        String comment2 ="";
-        if (scores[0] > 100) {
-            System.out.println("green");
-            int max = sortedScores[sortedScores.length - 1];
-            int max2 = sortedScores[sortedScores.length - 2];
-
-            if (max > 110) {
-                int result1st = e.indexOf(scores,max);
-                String kriterium1 = ingredients[result1st];
-                if (result1st != 0)
-                    comment1 =("Awesome, this meal contains a lot of " + kriterium1 + "!");
-            }
-            if (max2 > 100) {
-                int result2nd = e.indexOf(scores,max2);
-                if(result2nd == 0)
-                {
-                    max2 = sortedScores[sortedScores.length - 3];
-                    if(max2 > 100)
-                        result2nd = e.indexOf(scores,max2);
-                }
-                String kriterium2 = ingredients[result2nd];
-                if (result2nd != 0)
-                    comment2 = ("Great, this meal contains a lot of " + kriterium2 + "!");
-            }
-
-        } else {
-            if (scores[0] > 90) {
-                System.out.println("yellow");
-            } else {
-                System.out.println("red");
-            }
-            int min = sortedScores[0];
-            int min2 = sortedScores[1];
-            if (min < 87) {
-                //int result1st = Arrays.asList(scores).indexOf(min);
-                int result1st = e.indexOf(scores, min);
-                String kriterium1 = ingredientsUnhealthy[result1st];
-                if (result1st != 0) {
-                    if (result1st == 2)
-                        comment1 = ("Boo, this meal contains too much " + kriterium1 + "!");
-                    if (result1st == 4)
-                        comment1 = ("Oh no, this meal contains too many " + kriterium1 + "!");
-                    if (result1st == 3 || result1st == 5)
-                        comment1 = ("This meal contains not enough " + kriterium1 + "!");
-                    if (result1st == 1) {
-                        if (e.getDetails()[0] == 1) {
-                            comment1 = ("This meal has too many carbohydrates!");
-                        }
-                        if (e.getDetails()[1] == 1) {
-                            comment1 = ("This meal has too many fats!");
-                        }
-                        int[][] mahlzeit = e.getMahlzeit();
-                        int mealtime = 1; //Has to be replaced with actual mealtime
-                        String[] mealtype = {"breakfast", "lunch", "dinner", "snack"};
-                        comment1 = ("The optimal " + mealtype[mealtime] + " should only consist of a maximum proportion of " + mahlzeit[mealtime][3] + "% fat and at most " + mahlzeit[mealtime][4] + "% carbohydrates!");
-                    }
-                }
-            }
-
-
-            if (min2 < 87) {
-                int result2nd = e.indexOf(scores, min2);
-
-                if(result2nd == 0) {
-                    min2 = sortedScores[2] ;
-                    if(min2<87)
-                        result2nd = e.indexOf(scores,min2);
-                }
-                String kriterium2 = ingredientsUnhealthy[result2nd];
-
-                if (result2nd != 0) {
-
-                    if (result2nd == 2)
-                        comment2 = ("Bad news, this meal contains too much " + kriterium2 + "!");
-                    if (result2nd == 4)
-                        comment2 =("Bad news, this meal contains too many " + kriterium2 + "!");
-                    if (result2nd == 3 || result2nd == 5)
-                        comment2 =("Unfortunately, this meal contains not enough " + kriterium2 + "!");
-                }
-
-                if (result2nd == 1) {
-                    if (e.getDetails()[0] == 1) {
-                        comment2 =("This meal has too many carbohydrates!");
-                    }
-                    if (e.getDetails()[1] == 1) {
-                        comment2 =("This meal has too many fats!");
-                    }
-                    int[][] mahlzeit = e.getMahlzeit();
-                    int mealtime = 1; //Has to be replaced with actual mealtime
-                    String[] mealtype = {"breakfast", "lunch", "dinner", "snack"};
-                    comment2 =("The optimal " + mealtype[mealtime] + " should only consist of a maximum proportion of " + mahlzeit[mealtime][2] + "% fat and at most " + mahlzeit[mealtime][3] + "% carbohydrates!");
-                }
-
-
-            }
-        }
-        if(!comment1.isEmpty())
-            System.out.println(comment1); //you need to fetch only these 2 comments and display them on the screen
-        if(!comment2.isEmpty())
-            System.out.println(comment2);
-
-
-
-    }
-
-    private void getNutrition(final String foodName) {
-
-        JsonObject httpQuery = new JsonParser().parse(
-                "{" +
-                        "\"query\":" + "\"" + foodName + "\"" +
-                        ", " +
-                        "\"timezone\":\"US/Eastern\"" +
-                        "}").getAsJsonObject();
-
-        Callback<JsonObject> callbackGetNutrition = new Callback<JsonObject>() {
-            @Override
-            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                //JsonObject lresponse = response.body();
-                //System.out.println(response.body().toString());
-                String sApiValues = response.body().toString();
-
-
-                //System.out.println(b);
-                double[] apiValues = new double[32];
-                String a = "";
-                String[] splitApiValues = sApiValues.split("full_nutrients", -1);
-                String[] subHaupt = splitApiValues[0].split(",", -1);
-                String[] inhalt = {"nf_protein", "nf_total_fat", "nf_total_carbohydrate", "nf_sugars", "nf_dietary_fiber", "nf_saturated_fat"};
-                for (int i = 0; i < subHaupt.length; i++) {
-                    for (int j = 0; j < inhalt.length; j++) {
-                        if (subHaupt[i].contains(inhalt[j])) {
-                            if (subHaupt[i].substring(inhalt[j].length() + 3).equals("null"))
-                                apiValues[j] = -1;
-                            else
-                                apiValues[j] = Double.parseDouble(subHaupt[i].substring(inhalt[j].length() + 3));
-                        }
-                    }
-                }
-                String[] subExtra = splitApiValues[1].split("\\}", -1);
-                String[] inhaltExtra = {"attr_id\":645,", "attr_id\":646,"}; //645 monosaturated, 646 polysaturated
-                for (int i = 0; i < subExtra.length; i++) {
-                    for (int j = 0; j < inhaltExtra.length; j++) {
-                        if (subExtra[i].contains(inhaltExtra[j])) {
-                            if (subExtra[i].substring(inhaltExtra[j].length() + 11).equals("null"))
-                                apiValues[j + 6] = -1;
-                            else
-                                apiValues[j + 6] = Double.parseDouble(subExtra[i].substring(inhaltExtra[j].length() + 11));
-                        }
-                    }
-                }
-
-
-                //Test Code
-                String s = "";
-                System.out.println("Food result for: " + foodName);
-                System.out.println("The dish contains the following nutrients");
-                for (double i : apiValues)
-                    s += Double.toString(i) + "\n";
-                for (int i = 8; i < apiValues.length; i++)
-                    apiValues[i] = 0;
-                System.out.println(s);
-                Evaluator e = new Evaluator();
-                int[] preferences = {1, 1, 1, 1, 1};
-                System.out.println();
-                System.out.println("The dish receives the following scores");
-                int scores[] = e.evaluateDish(1, preferences, apiValues);
-                for (int i = 0; i < scores.length; i++)
-                    System.out.println(scores[i]);
-                System.out.println();
-                System.out.println("The dish receives the following colour");
-
-                getComment(scores,e); //This Mmethod got added, you just need the 2 Systemoutprinlns from the end of the method
-                if(scores[0] > 100)
-                    System.out.println("green");
-                if (scores[0] > 90) {
-                    System.out.println("yellow");
-                } else {
-                    System.out.println("red");
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<JsonObject> call, Throwable t) {
-                System.out.println(t.getMessage());
-            }
-        };
-
-        Call<JsonObject> callGetNutrition = apiInterface.getNutrition(httpQuery.toString());
-        callGetNutrition.enqueue(callbackGetNutrition);
-
-    }
 }
